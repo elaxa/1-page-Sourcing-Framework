faa5ea9e3a648b344375d0245ad98e0f5996d3fd
Rodion Yaryy <ryaryy@gmail.com>
2015-03-16 13:52:43 -0700

    add sleep to search

--- spec/isolation/search_page_spec.rb
+++ spec/isolation/search_page_spec.rb
@@ -4,6 +4,7 @@ describe "SearchPage" do
 
   it "should open search page" do
     @sourcing.browser.goto (@sourcing_url[:search])
+    sleep 5
     @sourcing.search_page.submit_btn.present?.should be true
   end
 
