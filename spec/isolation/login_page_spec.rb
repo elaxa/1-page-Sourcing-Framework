2599f4dbeecab689add35338f25eab81e3782bbd
Rodion Yaryy <ryaryy@gmail.com>
2015-03-16 13:57:02 -0700

    login page

--- spec/isolation/login_page_spec.rb
+++ spec/isolation/login_page_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe "LoginPage" do
   it "should fail because you haven't written any tests yet" do
-    expect(true).to be_true
+    expect(true).should be true
   end
 end
